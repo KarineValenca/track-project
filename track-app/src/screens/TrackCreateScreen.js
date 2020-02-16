39580661aa3c7c93dd936b28d5d82bc4c3b67ac9
Karine Valenca <valenca.karine@gmail.com>
2020-02-16 18:20:11 -0300

    using mock location and moving around with map

--- track-app/src/screens/TrackCreateScreen.js
+++ track-app/src/screens/TrackCreateScreen.js
@@ -1,25 +1,16 @@
 import '../_mockLocation'
-import React, { useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
-import { requestPermissionsAsync } from 'expo-location'
 import Map from '../components/Map'
+import { Context as LocationContext } from '../context/LocationContext'
+import useLocation from '../hooks/useLocation'
 
 const TrackCreateScreen = () => {
-    const [err, setErr] = useState(null)
+    const { addLocation } = useContext(LocationContext)
 
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync()
-        } catch (e) {
-            setErr(e)
-        }
-    }
-
-    useEffect(() => {
-        startWatching()
-    }, [])
+    const [err] = useLocation(addLocation)
 
     return (
         <SafeAreaView forceInset={{ top: 'always'}}>
