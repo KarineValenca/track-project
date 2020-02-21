f73308b008ef92b458c09c24e3e1e07e944b539a
Karine Valenca <valenca.karine@gmail.com>
2020-02-20 21:09:43 -0300

    saved track at database

--- track-app/src/api/tracker.js
+++ track-app/src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios'
+import { AsyncStorage } from 'react-native'
 
-export default axios.create({
-    baseURL: 'http://c70dbe52.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://091cf097.ngrok.io'
 })
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance
+
